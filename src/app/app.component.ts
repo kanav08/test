019b0a3e39ec52572e5efbc7819c11b6eecd56b8
kanav <kanav.kohli@infosys.com>
2019-09-08 23:11:30 +0530

    My Angular6 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import{ User } from './user-details/user';
+import { TestService } from './test.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basic';
+params:String="Demo";
+  /*
+  user:User;
+  constructor(){
+this.user= new User();
+this.user.name="Vanya";
+this.user.gender="Female";
+this.user.dob="03-Jun-2019";
+
+this.user.mobile=['99887766','99886644'];
+this.user.address="Ludhiana";
+  }
+  */
+
+ constructor(private serv:TestService, private http:HttpClient){
+   serv.printToConsole("Hello Kanav");
+
+   }
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { FirstComponent } from './first/first.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import { TestService } from './test.service';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ErrorComponent } from './error/error.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UpdateComponent } from './update/update.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+import { GetEmployeeComponent } from './get-employee/get-employee.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    UserDetailsComponent,
+    HomeComponent,
+    SettingsComponent,
+    ErrorComponent,
+    ProfileComponent,
+    UpdateComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    GetEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,ReactiveFormsModule
   ],
-  providers: [],
+  providers: [TestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
